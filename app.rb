e8294ab9fd588fb9b4e9dbe0f162459c87e28d15
Adrian Minissale <adrianminissale@gmail.com>
2015-12-22 10:47:14 -0300

    Initial app

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'cuba'
+require 'cuba/render'
+require 'yaml'
+require 'json'
+require 'open-uri'
+require 'haml'
+
+Cuba.plugin Cuba::Render
+Cuba.settings[:render][:template_engine] = 'haml'
+
+Cuba.use Rack::Session::Cookie, :secret => "$3cr3t$3ss|0n"
+
+Cuba.use Rack::Static,
+  root: "public",
+  urls: ["/assets"]
+
+Cuba.define do
+
+  on root do
+    render('home/index')
+  end
+
+end
---
+++ config.ru
@@ -0,0 +1,4 @@
+ROOT_DIR = File.expand_path(File.dirname(__FILE__)) + '/'
+
+require ROOT_DIR + 'app'
+run Cuba
